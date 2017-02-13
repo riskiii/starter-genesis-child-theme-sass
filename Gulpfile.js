5adb6b0ca9b2109c4717cd0979b14bba9eefbb25
Timothy Jensen <tim@timjensen.us>
2017-02-13 12:01:16 -0500

    Updated Gulpfile.js

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,17 +1,64 @@
 // Require our dependencies
-var gulp = require('gulp');
-var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var gulp = require('gulp');
+var notify = require('gulp-notify');
+var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+/**
+ * Handle errors and alert the user.
+ */
+function handleErrors () {
+    var args = Array.prototype.slice.call(arguments);
+
+    notify.onError({
+        title: 'Task Failed [<%= error.message %>',
+        message: 'See console.',
+        sound: 'Sosumi' // See: https://github.com/mikaelbr/node-notifier#all-notification-options-with-their-defaults
+    }).apply(this, args);
+
+    gutil.beep(); // Beep 'sosumi' again
+
+    // Prevent the 'watch' task from stopping
+    this.emit('end');
+}
 
 gulp.task('styles', function(){
-	return gulp.src('sass/style.scss')
-	.pipe(sass({
-		outputStyle: 'expanded', // Options: nested, expanded, compact, compressed
-		indentType: 'tab',
-		indentWidth: 1
-	})) // Converts Sass to CSS with gulp-sass
-    .pipe(gulp.dest('./'))
-	.pipe(browserSync.reload({stream: true}));
+    return gulp.src('assets/sass/*.scss')
+
+    // Deal with errors.
+        .pipe(plumber({ errorHandler: handleErrors }))
+
+        // Wrap tasks in a sourcemap.
+        .pipe(sourcemaps.init())
+
+        // Compile Sass using LibSass.
+        .pipe(sass({
+            errLogToConsole: true,
+            outputStyle: 'expanded', // Options: nested, expanded, compact, compressed
+            indentType: 'tab',
+            indentWidth: 1
+        }))
+
+        // Create sourcemap.
+        .pipe(sourcemaps.write())
+
+        // Create style.css.
+        .pipe(gulp.dest('./'))
+        .pipe(browserSync.stream());
+
+	// return gulp.src('assets/sass/style.scss')
+	// 	.pipe(sourcemaps.init())
+    //
+	// 	.pipe(sass({
+	// 		outputStyle: 'expanded', // Options: nested, expanded, compact, compressed
+	// 		indentType: 'tab',
+	// 		indentWidth: 1
+	// 	})) // Converts Sass to CSS with gulp-sass
+	// 	.pipe(sourcemaps.write())
+	// 	.pipe(gulp.dest('./'))
+	// 	.pipe(browserSync.reload({stream: true}));
 });
 
 // Gulp watch syntax
@@ -22,7 +69,7 @@ gulp.task('serve', function(){
 		proxy: "www.wpsite.dev"
     });
 
-	gulp.watch('sass/**/*.scss', ['styles']);
+	gulp.watch('assets/sass/**/*.scss', ['styles']);
 	gulp.watch('./**/*.php',browserSync.reload);
 });
 
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Starter Genesis Child Theme with Gulp & Sass
 
 Note: as this theme's stylesheet is generated from Sass, changes made directly to style.css will be overwritten when the Sass partials are compiled again.
+
+## Installation:
+From the theme's root directory, run the command `npm install` to download the theme's dependencies.
